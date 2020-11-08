f7b8a635c7aef5cc925858db1e560e0de141b590
Victor Quintanilha de Sousa <victorquintanilhasousa@gmail.com>
2020-11-08 03:42:55 -0400

    Exercicio 1008 - URI

---
+++ 1008.py
@@ -0,0 +1,7 @@
+n_funcionario = int(input())
+hr_trab = int(input())
+valor_hr = float(input())
+
+salario = hr_trab * valor_hr
+print('NUMBER = {}'.format(n_funcionario))
+print('SALARY = U$ %.2f' % salario)
